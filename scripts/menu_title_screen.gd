f36814795edcce931671236345d9f5b4d6b4be52
profgstv <gus.migueljorge@gmail.com>
2026-03-21 13:27:43 -0300

    Criação do protótipo Player

--- scripts/menu_title_screen.gd
+++ scripts/menu_title_screen.gd
@@ -3,14 +3,10 @@ extends Panel
 @onready var start_button = $Jogar
 @onready var quit_button = $Sair
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	start_button.grab_focus()
 	pass
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	if start_button.is_hovered():
 		start_button.grab_focus()
@@ -20,7 +16,7 @@ func _process(_delta):
 
 
 func _on_jogar_pressed():
-	get_tree().change_scene_to_file("res://scenes/title_screen.tscn")
+	get_tree().change_scene_to_file("res://scenes/templates/player.tscn")
 
 func _on_sair_pressed():
 	get_tree().quit()
